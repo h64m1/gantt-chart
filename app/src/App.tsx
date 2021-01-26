f1577a5527d680298fd8f1623565716a3d54f7c8
h64m1 <h64ms1@gmail.com>
2021-01-26 20:34:42 +0900

    [mod] reducerを分離 #12

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,92 +1,9 @@
 import React, { useEffect, useReducer } from 'react'
-import { Action, State, createTasks, createTask, getTaskKey } from './component/Types/Types'
+import { createTasks } from './component/Types/Types'
 import { ThisYearMonth } from './component/Date/Day'
 import GanttApp from './component/Gantt/GanttApp'
+import { reducer } from './reducer'
 import './App.css'
-
-const reducer = (state: State, action: Action) => {
-	switch (action.type) {
-		case 'yearMonth':
-			console.debug('処理年月変更', action.yearMonth)
-			return {
-				...state,
-				yearMonth: action.yearMonth,
-			}
-		case 'init':
-			return {
-				...state,
-				tasks: createTasks(),
-			}
-		case 'title': {
-			console.debug('タイトル変更', state)
-			const newTasks = { ...state.tasks }
-			newTasks.entities[action.id].title = action.title
-			return {
-				...state,
-				tasks: newTasks,
-			}
-		}
-		case 'task': {
-			console.debug('タスク変更', state)
-			// ONとOFFを切り替える
-			const taskStatus = [...state.tasks.entities[action.id].taskStatus]
-			taskStatus[action.column] = !taskStatus[action.column]
-			return {
-				...state,
-				tasks: {
-					...state.tasks,
-					entities: {
-						...state.tasks.entities,
-						[action.id]: {
-							...state.tasks.entities[action.id],
-							taskStatus: taskStatus,
-						},
-					},
-				},
-			}
-		}
-
-		// 行追加と削除
-		case 'addRow': {
-			const newTasks = { ...state.tasks }
-			console.debug('行追加:', action)
-			const ids = newTasks.ids
-			const entities = newTasks.entities
-
-			const key = getTaskKey(ids.length + 1)
-			ids.push(key)
-			entities[key] = createTask()
-
-			return {
-				...state,
-				tasks: {
-					ids: ids,
-					entities: entities,
-				},
-			}
-		}
-		case 'deleteRow': {
-			const newTasks = { ...state.tasks }
-			console.debug('行削除', action)
-			const ids = newTasks.ids
-			const entities = newTasks.entities
-
-			if (ids.length > 1) {
-				// 要素が1つの場合は削除しない
-				const key = getTaskKey(ids.length)
-				ids.pop()
-				delete entities[key]
-			}
-			return {
-				...state,
-				tasks: {
-					ids: ids,
-					entities: entities,
-				},
-			}
-		}
-	}
-}
 
 function App(): JSX.Element {
 	// 当日の日付で処理年月stateを初期化
